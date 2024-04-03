5e4fcbbc5e17ad77f19382ba85c3fde2aa87fac3
Paulo Engelke <p.engelke@e-mission.de>
2024-04-03 19:59:01 +0200

    add logos for desktop

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,40 @@
 <template>
   <div class="relative">
-    <div class="fixed z-10 flex flex-row">
-      <TikLogoN class="h-[150px] hidden md:block mt-5 ml-5"></TikLogoN>
+    <div class="fixed z-10">
+      <div
+        class="hidden md:flex mt-5 ml-5 justify-center flex-col items-center cursor-pointer"
+      >
+        <TikLogoN class="mb-5 h-[150px]"></TikLogoN>
+        <a
+          href="https://kultur.sachsen-anhalt.de/kultur-entdecken/kulturland-entdecken/industriekultur#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="./assets/lsa_logo_weiss.png"
+            style="max-height: 50px"
+            href=""
+          />
+        </a>
+        <a
+          href="https://moderndenken.sachsen-anhalt.de/moderne-denker"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="./assets/moderndenken_logo_weiss.png"
+            style="max-height: 50px"
+          />
+        </a>
+        <a
+          href="https://www.ferropolis.de/de/cms/_redaktionell/28/Industriekultur.html"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="mt-5"
+        >
+          <img src="./assets/nik_logo_weiss.png" style="max-height: 50px" />
+        </a>
+      </div>
       <!-- <h2 class="text-center text-2xl text-t-green m-auto p-l-4">
         Regionales Programm
       </h2> -->
